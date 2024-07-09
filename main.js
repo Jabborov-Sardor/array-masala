c38329a7cd649877cf5103d01c2e5f998c09ceb9
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-07-10 02:46:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const students = [
+  { name: "Baxrom", age: 21, grades: [2, 4, 5] },
+  { name: "Davron", age: 25, grades: [2, 4, 5] },
+  { name: "Ravshan", age: 23, grades: [4, 5] },
+];
+const student1Grade = function (grades) {
+  let sum = 0;
+  for (let i = array; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+console.log(student1Grade(students.grades));
