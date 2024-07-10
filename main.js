263d25233f4fad385fbecc45b62d3a40df7f0443
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-07-10 13:07:25 +0500

    end

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-const students = [
-  { name: "Baxrom", age: 21, grades: [2, 4, 5] },
-  { name: "Davron", age: 25, grades: [2, 4, 5] },
-  { name: "Ravshan", age: 23, grades: [4, 5] },
+let students = [
+  { name: "azamjon", age: 17, grades: [8, 9, 6] },
+  { name: "umarov", age: 18, grades: [8, 7, 2] },
+  { name: "azam", age: 19, grades: [8, 8, 8] },
 ];
-const student1Grade = function (grades) {
-  let sum = 0;
-  for (let i = array; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-};
-console.log(student1Grade(students.grades));
+students.forEach((student) => {
+  let baho =
+    student.grades.reduce((sum, grade) => sum + grade, 0) /
+    student.grades.length;
+  console.log(`${student.name} o'rtacha bahosi ${baho.toFixed(2)}`);
+});
+
+////// 2 masala /////
+const books = [
+  { title: "salom", year: 2004, author: "sarvar", ganre: "detektiv" },
+  { title: "jangari", year: 2018, author: "anvar", ganre: "tragediya" },
+  { title: "kulgi", year: 2013, author: "baxtiyor", ganre: "komediya" },
+];
+function book1(books1) {
+  return books1.filter((i) => i.year > 2010);
+}
+
+function book2(books2) {
+  return books2.filter((a) => a.year < 2010);
+}
+
+let name1 = book1(books);
+console.log("yangi kitoblar");
+console.log(name1);
+
+let name2 = book2(books);
+console.log("eski kitoblar");
+console.log(name2);
